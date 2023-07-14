625eb9dbd07a4f1c82a8dfd1493b206b9dfdb1a1
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-14 18:39:10 -0400

    commit tree fe92285e91309f7772a290d1537a92003e6b32fd

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,50 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    current_node = nil
+    nodes_to_visit = @root
+    
+    while nodes_to_visit != nil
+      current_node = nodes_to_visit
+      if value < current_node.value
+        nodes_to_visit = current_node.left
+      elsif value > current_node.value
+        nodes_to_visit = current_node.right
+      else
+        return current_node
+      end
+    end
+
+    return nil
   end
 
   def insert(value)
-    # your code here
+    node = Node.new(value)
+    if @root == nil
+      @root = node
+      return @root
+    end
+
+    last = nil
+    nodes_to_visit = @root
+
+    while nodes_to_visit != nil
+      if node.value < nodes_to_visit.value
+        last = nodes_to_visit
+        nodes_to_visit  = nodes_to_visit.left
+      elsif node.value > nodes_to_visit.value
+        last = nodes_to_visit
+        nodes_to_visit = nodes_to_visit.right
+      else
+        return nil
+      end
+    end
+
+    if node.value < last.value 
+      last.left = node
+    elsif node.value > last.value
+      last.right = node
+    end
   end
 
 end 
